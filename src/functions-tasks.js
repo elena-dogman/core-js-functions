d245be53a67e9098480a24800d7111d43c7d7e23
elena-dogman <dogman.choli@gmail.com>
2024-04-11 07:50:31 +0300

    feat: solve memoize problem

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -107,8 +107,17 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+
+  return function memoizeArgs(...args) {
+    const key = JSON.stringify(args);
+
+    if (!cache[key]) {
+      cache[key] = func.apply(args, this);
+    }
+    return cache[key];
+  };
 }
 
 /**
